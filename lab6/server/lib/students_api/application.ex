511d6b9dadd7be8ce40b5e533979ad4c45c09a9f
Oskar Legner <oskarlegner@gmail.com>
2022-01-13 21:48:10 +0100

    Add initial data

--- lab6/server/lib/students_api/application.ex
+++ lab6/server/lib/students_api/application.ex
@@ -6,12 +6,18 @@ defmodule StudentsApi.Application do
   use Application
 
   alias StudentsApi.DataAgent
+  alias StudentsApi.Student
 
   @impl true
   def start(_type, _args) do
     children = [
       # Start HTTP server
-      {DataAgent, []},
+      {DataAgent,
+       [
+         %Student{index: 213_456, first_name: "Anna", last_name: "Nowak"},
+         %Student{index: 213_457, first_name: "Jan", last_name: "Kowalski"},
+         %Student{index: 213_458, first_name: "Zenon", last_name: "Zawada"}
+       ]},
       {Plug.Cowboy, scheme: :http, plug: StudentsApi.Endpoint, options: [port: 4000]}
     ]
 
