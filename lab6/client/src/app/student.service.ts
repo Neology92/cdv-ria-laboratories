9a2011229a586d77f64cc38a1cf806d6ed1cb1e8
Oskar Legner <oskarlegner@gmail.com>
2022-01-22 12:45:44 +0100

    Fill sudents service methods

--- lab6/client/src/app/student.service.ts
+++ lab6/client/src/app/student.service.ts
@@ -3,6 +3,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Student } from './student';
 import { Observable, of } from 'rxjs';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'applicaiton/json' }),
+};
+
 @Injectable({
   providedIn: 'root',
 })
@@ -18,5 +22,20 @@ export class StudentService {
   getStudent(id: number): Observable<Student> {
     const url = `${this.studentsApiUrl}/${id}`;
     return this.http.get<Student>(url);
+  }
+
+  updateStudent(student: Student): Observable<any> {
+    const url = `${this.studentsApiUrl}/${student.id}`;
+    return this.http.put<Student>(url, student, httpOptions);
+  }
+
+  createStudent(student: Student): Observable<Student> {
+    return this.http.post<Student>(this.studentsApiUrl, student, httpOptions);
+  }
+
+  deleteStudent(student: Student | number): Observable<Student> {
+    const id = typeof student === 'number' ? student : student.id;
+    const url = `${this.studentsApiUrl}/${id}`;
+    return this.http.delete<Student>(url, httpOptions);
   }
 }
